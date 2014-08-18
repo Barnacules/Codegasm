f826d719c0daf85ec512fe8eaab1f6d1d1f27150
Barnacules <jerryberg@gmail.com>
2014-08-18 14:34:30 -0700

    #Codegasm Projects
    Source code for current #Codegasm projects

---
+++ Test App 1 - Codegasm/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Namespace
+namespace JerrysCoolAppCode
+{
+    // Class (contains functionality)
+    class Program
+    {
+        // Function (entry point)
+        static void Main(string[] args)
+        {
+            PrintFooToScreen100Times();
+
+            Console.WriteLine("Type a number, any number?");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            if (keyInfo.KeyChar == 'a')
+            {
+                Console.WriteLine("That is not a number! KNOCK IT OFF NOW!");
+            }
+            else
+            {
+                Console.WriteLine("Did you type {0}", keyInfo.KeyChar.ToString());
+            }
+        }
+
+        /// <summary>
+        /// All this function does is print Foo to the screen
+        /// </summary>
+        static void PrintFooToScreen()
+        {
+            Console.WriteLine("Foo");
+        }
+
+        static void PrintFooToScreen100Times()
+        {
+            for(int counter=0; counter <= 100; counter++)
+            {
+                PrintFooToScreen();
+            }
+        }
+    }
+}
---
+++ Test App 2 - Demo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Added references
+// System.Web.Dll
+
+// Added namespaces
+using System.Web;
+using System.Threading;
+
+namespace TestApp2Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We want to save the current console color so we can restore it later
+            ConsoleColor oldColor = Console.ForegroundColor;
+
+            // Set the new console color to Green
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            // Tell everyone what this program is when it starts
+            Console.WriteLine("Welcome to #Codegasm episode 2 - Magic 8 ball app");
+
+            Random random = new Random();
+            while (true)
+            {
+                Console.Write("Ask any question: ");
+                string question = Console.ReadLine();
+                if( question.ToLower() == "quit")
+                {
+                    break;
+                }
+                Console.Write("Answer: ");
+                switch( random.Next(5) )
+                {
+                    case 0:
+                        Console.WriteLine("Yes!");
+                        break;
+                    case 1:
+                        Console.WriteLine("Hell No!");
+                        break;
+                    case 2:
+                        Console.WriteLine("Probably.");
+                        break;
+                    case 3:
+                        Console.WriteLine("WTF do you think?");
+                        break;
+                    case 4:
+                        Console.WriteLine("I don't understand the question?!?!");
+                        break;
+                }
+            }
+
+            // Restore the old foreground color
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
